0a9ca0fa010de5366a4b280b3e3cd27494c67fe2
katrajh <katarina.rajh@gmail.com>
2021-02-16 00:15:44 +0100

    delujoče delovanje dodajanja kontaktov.. SPA ready

--- server/index.js
+++ server/index.js
@@ -11,5 +11,19 @@ app.use(cors());
 
 const contacts = require('./routes/api/contacts');
 app.use('/api/contacts', contacts);
+
+const users = require('./routes/api/users');
+app.use('/api/users', users);
+
+
+// Handle production (runing on server)
+if(process.env.NODE_ENV == 'production') {
+    // Static folder 
+    app.use(express.static(__dirname + '/public'));
+
+    // Handle SPA (single page application)
+    app.get(/.*./, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server started on port "+port));
